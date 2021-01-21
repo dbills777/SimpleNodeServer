66367e87f9b5d4d889a760558448be09c5c04ecc
Derrick Bills <dbills777@gmail.com>
2021-01-21 14:47:43 -0700

    static html page & json data

---
+++ routes/route.app.js
@@ -0,0 +1,22 @@
+//imports
+import express from 'express';
+import { Router } from 'express';
+//export appRouter instance
+export const appRouter = Router();
+//tell appRouter to serve public folder
+appRouter.use(express.static('public'));
+//tell app what file to server from the public folder
+appRouter.get('/', (req, res) => {
+  res.sendFile('./index.html');
+});
+appRouter.get('/json', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write(
+    JSON.stringify({
+      message: 'Hello World',
+      email: 'someemail@address.com',
+      data: 'some more json data',
+    })
+  );
+  res.end();
+});
---
+++ server.js
@@ -0,0 +1,12 @@
+//imports
+import express from 'express';
+import { appRouter } from './routes/route.app.js';
+
+const app = express();
+const port = process.env.PORT || 5000;
+//tell app to use routes
+app.use('/', appRouter);
+//listen on assigned env port or default port
+app.listen(process.env.PORT || port, () =>
+  console.log(` App LIstening at http://localhost:${port}`)
+);
