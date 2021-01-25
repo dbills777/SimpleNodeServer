20797c2a8cf4662acebdf0a4ff50af9cd7f9163f
Derrick Bills <dbills777@gmail.com>
2021-01-25 14:49:04 -0700

    simple server assignent status

--- routes/route.app.js
+++ routes/route.app.js
@@ -1,14 +1,18 @@
 //imports
 import express from 'express';
 import { Router } from 'express';
+
 //export appRouter instance
 export const appRouter = Router();
+
 //tell appRouter to serve public folder
 appRouter.use(express.static('public'));
+
 //tell app what file to server from the public folder
 appRouter.get('/', (req, res) => {
   res.sendFile('./index.html');
 });
+// "/api" Route  that returns a JSON object
 appRouter.get('/json', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(
--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 import express from 'express';
 import { appRouter } from './routes/route.app.js';
 
+// app & port
 const app = express();
 const port = process.env.PORT || 5000;
+
 //tell app to use routes
 app.use('/', appRouter);
+
 //listen on assigned env port or default port
 app.listen(process.env.PORT || port, () =>
   console.log(` App LIstening at http://localhost:${port}`)
