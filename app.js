9e8ab3870498df2b3995b88aeef2d8dac90e56a0
Derrick Bills <dbills777@gmail.com>
2021-02-02 20:42:06 -0700

    cleanup of api.route.js
    api.route.js refactoring/cleanup

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 //imports
 import express from 'express';
-import { appRouter } from './routes/api.route.js';
+import { apiRouter } from './routes/api.route.js';
 
 // app & port
 const app = express();
 const port = process.env.PORT || 5000;
 
+//tell apiRouter to serve public folder
+app.use(express.static('public'));
+
 //tell app to use routes
-app.use('/', appRouter);
+app.use('/api', apiRouter);
 
 //listen on assigned env port or default port
-
 app.listen(process.env.PORT || port, () =>
   console.log(` App LIstening at http://localhost:${port}`)
 );
--- routes/api.route.js
+++ routes/api.route.js
@@ -1,19 +1,11 @@
 //imports
-import express from 'express';
 import { Router } from 'express';
 
 //export appRouter instance
-export const appRouter = Router();
+export const apiRouter = Router();
 
-//tell appRouter to serve public folder
-appRouter.use(express.static('public'));
-
-//tell app what file to server from the public folder
-appRouter.get('/', (req, res) => {
-  res.sendFile('./index.html');
-});
 // "/api" Route  that returns a JSON object
-appRouter.get('/api', (req, res) => {
+apiRouter.get('/', (req, res) => {
   res.json({
     message: 'Hello World',
     email: 'YourEmail@SomeAddress.com',
