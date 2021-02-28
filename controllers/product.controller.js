e43a7bf39bb144259366fd07113ea41c6ed10a67
Derrick Bills <dbills777@gmail.com>
2021-02-27 19:03:40 -0700

    getone product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -11,18 +11,36 @@ export const postAddProduct = (req, res) => {
   product.save();
   res.json(product);
 };
-
+// Example of other method for making requests
+export const getProductsThen = (req, res) => {
+  Product.find()
+    .then((products) => {
+      res.json(products);
+    })
+    .catch((err) => log(err));
+};
 export const getAllProducts = async (req, res) => {
   const products = await Product.find();
   res.json(products);
+};
+export const getProductByID = async (req, res) => {
+  const productID = req.body.productID;
+  try {
+    const product = await Product.findById(productID);
+    if (!product) return res.status(400)({ Message: 'Item with that productID is not found' });
+    console.log(`Product id: ${productID}`);
+    res.json(product)
+  } catch (error) {
+    res.status(400).json({ Message: `Can't find Product ${err}` });
+  }
 };
 
 export const deleteProductByID = async (req, res) => {
   const productID = req.body.productID;
   try {
-    const deletProduct = await Product.findByIdAndRemove(productID);
-    if (!deletProduct) return res.status(400)({ Message: 'Item with that productID is not found' });
-    console.log('Deleted: ${productID}');
+    const deleteProduct = await Product.findByIdAndRemove(productID);
+    if (!deleteProduct) return res.status(400)({ Message: 'Item with that productID is not found' });
+    console.log('Deleted: ${deleteProduct}');
     res.sendStatus(200);
   } catch (error) {
     res.status(400).json({ Message: `Invalid Product ${err}` });
--- routes/product.route.js
+++ routes/product.route.js
@@ -2,7 +2,7 @@
 import { Router } from 'express';
 
 // use product.conroller for exporting functions
-import { postAddProduct, getAllProducts, deleteProductByID } from '../controllers/product.controller.js';
+import { postAddProduct, getAllProducts, deleteProductByID, getProductsThen, getProductByID } from '../controllers/product.controller.js';
 
 //export appRouter instances
 export const productRouter = Router();
@@ -10,5 +10,8 @@ export const productRouter = Router();
 //TODO  could add more paths for /api route here
 // "/" Route  that returns a JSON object
 productRouter.post('/', postAddProduct);
+// extra example of other method of getting products.
+productRouter.get('/then', getProductsThen);
 productRouter.get('/', getAllProducts);
+productRouter.get('/getone', getProductByID);
 productRouter.delete('/', deleteProductByID);
